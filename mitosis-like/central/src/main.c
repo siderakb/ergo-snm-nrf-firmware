7cff9abcb2b9f7d5dda21a015c503f618dc8dce3
ziteh <honmonoh@gmail.com>
2024-02-02 23:00:01 +0800

    Add debug print

--- mitosis-like/central/src/main.c
+++ mitosis-like/central/src/main.c
@@ -135,10 +135,25 @@ void qmk_uart_send(uint8_t *keymatrix, uint8_t *mouse)
   qmk_uart_send_bytes(keymatrix, ROW_COUNT * 2);
 
   qmk_uart_send_bytes(mouse, 6);
-  // LOG_DBG("X: 0x%02X%02X. Y:0x%2X%2X", mouse[0], mouse[1], mouse[2], mouse[3]);
 
   uint8_t end[1] = {EOT};
   qmk_uart_send_bytes(end, 1);
+
+  LOG_DBG("%02X %02X %02X %02X %02X | %02X %02X %02X %02X %02X, X: 0x%02X%02X, Y: 0x%02X%02X",
+          keymatrix[0],
+          keymatrix[1],
+          keymatrix[2],
+          keymatrix[3],
+          keymatrix[4],
+          keymatrix[5],
+          keymatrix[6],
+          keymatrix[7],
+          keymatrix[8],
+          keymatrix[9],
+          mouse[0],
+          mouse[1],
+          mouse[2],
+          mouse[3]);
 }
 
 /**
@@ -255,20 +270,20 @@ void gzll_rx_result_handler(struct gzll_rx_result *rx_result)
     {
       offset = ROW_COUNT;
       memcpy(raw_mouse, data_payload + ROW_COUNT, 6);
-      LOG_DBG("L: %d, x: 0x%02X%02X, y: 0x%02X%02X, v: 0x%02X%02X, E: 0x%02X",
-              data_payload_length,
-              data_payload[ROW_COUNT],
-              data_payload[ROW_COUNT + 1],
-              data_payload[ROW_COUNT + 2],
-              data_payload[ROW_COUNT + 3],
-              data_payload[ROW_COUNT + 4],
-              data_payload[ROW_COUNT + 5],
-              data_payload[ROW_COUNT + 6]);
+      // LOG_DBG("L: %d, x: 0x%02X%02X, y: 0x%02X%02X, v: 0x%02X%02X, E: 0x%02X",
+      //         data_payload_length,
+      //         data_payload[ROW_COUNT],
+      //         data_payload[ROW_COUNT + 1],
+      //         data_payload[ROW_COUNT + 2],
+      //         data_payload[ROW_COUNT + 3],
+      //         data_payload[ROW_COUNT + 4],
+      //         data_payload[ROW_COUNT + 5],
+      //         data_payload[ROW_COUNT + 6]);
     }
     else if (data_payload_length == (ROW_COUNT + 1)) /* Left. */
     {
       offset = 0;
-      LOG_DBG("Left");
+      // LOG_DBG("Left");
     }
     else
     {
